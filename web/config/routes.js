3a4cefa01e63a61c833caff27bdb4b5916d6eca9
Elionai Moura Cordeiro <eli.embits@gmail.com>
2019-01-25 12:12:18 -0300

    Instalando configurações do ambiente

--- web/config/routes.js
+++ web/config/routes.js
@@ -3,19 +3,16 @@
 * @version 1.0.0
 * @description Router module for outgo_test app
 */
-
 const express = require( 'express' );
 const router = express.Router();
-//const categories = require('../app_modules/categories.js');
-//const movies = require('../app_modules/movies.js');
 
-router.get('/', (req, res) => res.json({ message: '_FNORD » OnixxApp is up and running!' }));
+router.get('/', (req, res, next) => res.json(
+  { message: '_FNORD » OnixxApp is up and running!' }
+));
 
-// router.get('/categories/populate', categories.populate);
-// router.get('/categories/:id?', categories.list);
-//
-// router.get('/movies/populate', movies.populate);
-// router.get('/movies/:id?', movies.list);
+// Any other GET request will fall here
+router.get('*', (req, res, next) => res.json(
+  { message: '_FNORD » It is a ∑ꞦꞦꝊꝶ <404> message::' }
+));
 
-router.get('*', (req, res) => res.send('//TODO: página de erro 404!'));
 module.exports = router;
